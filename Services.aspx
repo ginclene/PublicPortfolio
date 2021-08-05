d493ca474db49d2964dfd23305318f8835e69e24
ginclene <62023050+ginclene@users.noreply.github.com>
2021-08-05 14:17:22 -0400

    Part of the admin page for the Custodians of Oxford Project
    Part of the admin page to add/alter services and subscriptions for the Custodians of Oxford Project

---
+++ Services.aspx
@@ -0,0 +1,177 @@
+﻿<%@ Page Title="Services" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Services.aspx.cs" Inherits="CustodiansOfOxford.Services" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <script src="Scripts/moment-with-locales.js"></script>
+    <script src="Scripts/jquery-3.3.1.min.js"></script>
+    <script src="Scripts/moment.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/bootstrap-datetimepicker.js"></script>
+    <link href="Content/bootstrap-datetimepicker.css" rel="stylesheet" />
+
+    <!--
+        Page for admins only to update services
+        Can add, remove, and change services
+    -->
+    <h1>Services</h1>
+    <br />
+
+    <!-- Block to add new service -->    
+    <div id="newService">
+        <h2>Add a Service</h2> <br />
+        <!-- New Service Name and Type -->
+        <label for="ServiceName"> Service Name: </label>
+        <input type="text" class="form-control" name="ServiceName" id="ServiceName" placeholder="Enter name of service" runat="server" />
+        <input type="radio" name="serType" id="Cleaning" runat="server" value="Cleaning" />
+        <label class="form-check-label" for="Cleaning"> Cleaning </label>
+        <input type="radio" name="serType" id="Party" runat="server" value="Party" />
+        <label class="form-check-label" for="Party"> Party </label>
+        <input type="radio" name="serType" id="Other" runat="server" value="Other" />
+        <label class="form-check-label" for="Other"> Other </label>
+         <br />
+        <!-- New Service Image -->
+        <label for="serImage"> Image: </label>
+        <input type="file" name="serImage" runat="server" id="serImage" />
+        <br />
+        <!-- New Service Description -->
+        <input type="text" class="form-control" name="serDescription" id="serDescription" placeholder="Enter a Description of the service" runat="server" />
+        <br />
+        <!-- New Service Cost -->
+        <label for="baseCost"> Base Cost: </label>
+        <input type="number" name="baseCost" id="baseCost" runat="server" />
+        <!-- New Service Time properties -->
+        <label for="minLength"> Minimum time of service: </label>
+        <input type="number" name ="minLength" id="minLength" runat="server" />
+        <label for="maxLength"> Maximum time for service: </label>
+        <input type="number" name ="maxLength" id="maxLength" runat="server" />
+        <br />
+        <!-- Submit Button -->
+        <asp:Button ID="submit" runat="server" Text="Submit" CssClass="btn btn-primary" OnClick="addService" />
+    </div>
+    <br />
+    <br />
+    <!-- Remove Service Block -->
+    <div id="RemoveService">
+        <h2> Remove a Service</h2><br />
+
+        <!-- Select Service to be Removed -->
+        Find the Service:
+        <asp:DropDownList ID="ServiceNameDropDown" AutoPostBack="True" OnSelectedIndexChanged="viewSingleService" runat="server">
+        </asp:DropDownList>
+        <!-- Display Selected Services to be removed -- Can be Multiple -->
+        <asp:Label ID="ServiceRemovalSelection" runat="server" Text=""></asp:Label>
+        <!-- Submit Button -->
+        <asp:Button ID="submitR" runat="server" Text="Submit" CssClass="btn btn-primary" OnClick="ConfirmDeletion" />
+        <br />
+    </div>
+    <!-- Confirm Deletion Prompt After Submit -->
+    <div id="Confirmation" runat="server">
+        Confirm Deletion
+        <asp:Button ID="Confirm" runat="server" Text="Yes" CssClass="btn btn-primary" OnClick="deleteService" />
+        <asp:Button ID="Deny" runat="server" Text="No" CssClass="btn btn-primary" OnClick="CancelDeletion" />
+    </div>
+    <br />
+    <br />
+    <!-- Find Service to Change -->
+    <div id="FindService">
+       <h2> Change a Service </h2>
+
+        Find the Service:
+        <asp:DropDownList ID="ServiceNameF" AutoPostBack="True" OnSelectedIndexChanged="FindService" runat="server">
+            
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="lblData" runat="server" Text=""></asp:Label>
+    </div>
+
+    <!-- Select Attribute of Selected Service to be Changed -->
+    <div id="ChangeService">
+
+        Select the field you would like to change:
+        <asp:DropDownList ID="fieldchanged" AutoPostBack="True" OnSelectedIndexChanged="ChangeSelection" runat="server">
+            <asp:ListItem Selected="True" Value="serName"> Name </asp:ListItem>
+            <asp:ListItem Value="serType"> Type </asp:ListItem>
+            <asp:ListItem Value="serImage"> Image </asp:ListItem>
+            <asp:ListItem Value="serDescription"> Description </asp:ListItem>
+            <asp:ListItem Value="serBaseCost"> Base Cost </asp:ListItem>
+            <asp:ListItem Value="minLenOfService"> Minimum Service Length </asp:ListItem>
+            <asp:ListItem Value="maxLenOfService"> Maximum Service Length </asp:ListItem>
+        </asp:DropDownList>
+
+        <asp:Label ID="servicefield" runat="server" Text=""></asp:Label>
+    </div>
+
+    <!-- Change Selected Attribute -->
+    <div id="ChangeName" runat="server">
+        <label for='NewValue'> New Service Name: </label>
+        <input type = 'text' class='form-control' name='ServiceNameC' id='NewName' placeholder='Enter new name of service' runat='server' />
+    </div>
+    <div id="ChangeType" runat="server">
+        <input type='radio' name='serType' id='CleaningC' runat='server' value='Cleaning' />
+        <label class='form-check-label' for='CleaningC'> Cleaning</label>
+        <input type = "radio" name="serType" id="PartyC" runat="server" value="Party" />
+        <label class="form-check-label" for="Party"> Party</label>
+        <input type = "radio" name="serType" id="OtherC" runat="server" value="Other" />
+        <label class="form-check-label" for="OtherC"> Other</label>
+    </div>
+    <div id="ChangeImg" runat="server">
+        <label for='serImageC'> New Image: </label>
+        <input type = 'file' name = 'serImageC' runat = 'server' id = 'NewImage' /> 
+    </div>
+    <div id="ChangeDescription" runat="server">
+        <label for='NewValue'> New Service Description: </label>
+        <input type = 'text' class='form-control' name='ServiceDescriptionC' id='NewDescription' placeholder='Enter new Description' runat='server' />
+    </div>
+    <div id="ChangeBaseCost" runat="server">
+        <label for='NewValue'> New Base Cost: </label>
+        <input type = 'number' name = 'baseCostC' id = 'NewCost' runat = 'server' /> 
+    </div>
+    <div id="ChangeMin" runat="server">
+        <label for='NewValue'> New Minimum Length: </label>
+        <input type = 'number' name = 'minLenC' id = 'NewMin' runat = 'server' /> 
+    </div>
+    <div id="ChangeMax" runat="server">
+        <label for='maxLenC'> New Maximum Length: </label>
+        <input type = 'number' name = 'maxLenC' id = 'NewMax' runat = 'server' /> 
+    </div>
+    <!-- Submit Changes -->
+    <div id="ChangeButtonDiv" runat="server">
+      <asp:Button ID="ChangeButton" runat="server" Text="Submit Changes" CssClass="btn btn-primary" OnClick="updateService" />
+    </div>
+
+    <br />
+    <h1>Subscriptions</h1>
+
+    <br />
+
+    <!-- Add Subscription -->
+    <h2>Add a Subscription</h2>
+    <div id="newSubscription">
+    <label for="SubName"> Service Name: </label>
+        <input type="text" class="form-control" name="SubName" id="SubName" placeholder="Enter name of subscription" runat="server" />
+        <input type="radio" name="subType" id="subCleaning" runat="server" value="Cleaning" />
+        <label class="form-check-label" for="subCleaning"> Cleaning </label>
+        <input type="radio" name="subType" id="subParty" runat="server" value="Party" />
+        <label class="form-check-label" for="subParty"> Party </label>
+        <input type="radio" name="subType" id="subOther" runat="server" value="Other" />
+        <label class="form-check-label" for="subOther"> Other </label>
+         <br />
+        <label for="serImage"> Image: </label>
+        <input type="file" name="subImage" runat="server" id="subImg" />
+        <br />
+        <input type="text" class="form-control" name="subDescription" id="subDescription" placeholder="Enter a Description of the subscription" runat="server" />
+        <br />
+        <label for="weeklyCost"> Weekly Cost: $</label>
+        <input type="number" name="weeklyCost" id="weeklyCost" runat="server" />
+        <asp:Button ID="subsubmit" runat="server" Text="Submit" CssClass="btn btn-primary" OnClick="addSubscription" />
+    </div>
+    <br />
+    <br />
+
+    <!-- View Currently Booked Services -->
+    <div id="bookedServices">
+        <h1>Booked Services</h1>
+        <asp:GridView ID="BookedGrid" runat="server"></asp:GridView>
+    </div>
+    
+</asp:Content>
---
+++ Services.aspx.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CustodiansOfOxford.dsTableAdapters;
+
+namespace CustodiansOfOxford
+{
+    public partial class Services : System.Web.UI.Page
+    {
+        // Checks User and populates page
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         //   if (!User.IsInRole("Admin")) Response.Redirect("Default.aspx");
+            ChangeName.Visible = false;
+            ChangeDescription.Visible = false;
+            ChangeType.Visible = false;
+            ChangeMax.Visible = false;
+            ChangeMin.Visible = false;
+            ChangeImg.Visible = false;
+            ChangeBaseCost.Visible = false;
+            Confirmation.Visible = false;
+            viewAllServiceNames();
+            viewAllBookings();
+        }
+
+        // Function to handle adding a service
+        protected void addService(object sender, EventArgs e)
+        {
+            // new service type
+            string serviceType = "";
+            if (Cleaning.Checked)
+            {
+                serviceType = "cleaning";
+            }
+            else if (Party.Checked)
+            {
+                serviceType = "party";
+            }
+            else
+            {
+                serviceType = "other";
+            }
+
+            // new service image
+            string imageLocation = "pictures/" + serImage.Value;
+
+            // insert new service into database
+            string finalconnect = string.Empty;
+            finalconnect = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(finalconnect))
+            {
+                string strQuery = "INSERT INTO tblService (serName, serType, serImage, serDescription, serBaseCost," +
+                    " minLenOfService, maxLenOfService)" +
+                    " VALUES (@ServiceName, @serviceType, @serImage, @serDescription, @baseCost, @minLength, @maxLength)";
+                using (SqlCommand command = new SqlCommand(strQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@ServiceName", ServiceName.Value);
+                    command.Parameters.AddWithValue("@serviceType", serviceType);
+                    command.Parameters.AddWithValue("@serImage", imageLocation);
+                    command.Parameters.AddWithValue("@serDescription", serDescription.Value);
+                    command.Parameters.AddWithValue("@baseCost", Int32.Parse(baseCost.Value));
+                    command.Parameters.AddWithValue("@minLength", Int32.Parse(minLength.Value));
+                    command.Parameters.AddWithValue("@maxLength", Int32.Parse(maxLength.Value));
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    if (result < 0)
+                    {
+                        System.Diagnostics.Debug.WriteLine("whats the problem bobby");
+                    }
+                    
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+            string folderPath = Server.MapPath("~/pictures/");
+            string fileName = serImage.Value;
+            string imagePath = folderPath + fileName;
+        }
+
+        // function to handle deleting a service
+        protected void deleteService(object sender, EventArgs e)
+        {
+            // retrieve selected service from dropdown
+            string name = ServiceNameDropDown.SelectedValue;
+
+            // remove selected service from database
+            string finalconnect = string.Empty;
+            finalconnect = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(finalconnect))
+            {
+                string strQuery = "Delete From tblService Where serName='" + name + "'";
+                using (SqlCommand command = new SqlCommand(strQuery, connection))
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    // prints if error
+                    if (result < 0)
+                    {
+                        System.Diagnostics.Debug.WriteLine("whats the problem bobby");
+                    }       
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+        // retrieves all service names from database
+        protected void viewAllServiceNames()
+        {
+            string allServices = "";
+            foreach (ds.GetServicesRowRow r in (new dsTableAdapters.GetServicesRowTableAdapter()).GetData())
+            {
+                allServices = r.serName;
+                ServiceNameDropDown.Items.Add(allServices);
+                ServiceNameF.Items.Add(allServices);
+            }
+        }
+
+        // retrieves a single selected service
+        protected void viewSingleService(object sender, EventArgs e)
+        {
+            string name = ServiceNameDropDown.SelectedValue;
+            string capitalized = char.ToUpper(name[0]) + name.Substring(1);
+            string data = "";
+            data = "<h4>" + capitalized + "</h4><br/><table width='200' class='table'><thead><tr><th></th><th>Service name</th>";
+            data += "<th>Base Cost ($)</th><th>Service Type</th><th>Service Description</th><th>Min Length</th><th>Max Length</th></tr></thead><tbody>";
+            foreach (ds.GetServicesRowRow r in (new dsTableAdapters.GetServicesRowTableAdapter()).GetData())
+            {
+                if ((r.serName).Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    data += "<tr><td><img width='100' height='100' src='" + r.serImage + "'" + r.serName + "' /></td><td>" + r.serName + "</td><td>" + r.serBaseCost + "</td><td>" + r.serType + "</td><td>"
+                        + r.serDescription + "</td><td>" + r.minLenOfService + "</td><td>" + r.maxLenOfService + "</td></tr>";
+                }
+            }
+            ServiceRemovalSelection.Text += data + "</tbody></table>";
+        }
+
+        // prompts the user to confirm deletion
+        protected void ConfirmDeletion(object sender, EventArgs e)
+        {
+            Confirmation.Visible = true;
+        }
+
+        // hides confirmation prompt on cancel
+        protected void CancelDeletion(object sender, EventArgs e)
+        {
+            Confirmation.Visible = false;
+        }
+
+        // finds a service in the database
+        protected void FindService(object sender, EventArgs e)
+        {
+            string name = ServiceNameF.SelectedValue;
+            string capitalized = char.ToUpper(name[0]) + name.Substring(1);
+            string data = "";
+            data = "<h4>" + capitalized + "</h4><br/><table width='200' class='table'><thead><tr><th></th><th>Service name</th>";
+            data += "<th>Base Cost ($)</th><th>Service Type</th><th>Service Description</th><th>Min Length</th><th>Max Length</th></tr></thead><tbody>";
+            foreach (ds.GetServicesRowRow r in (new dsTableAdapters.GetServicesRowTableAdapter()).GetData())
+            {
+                if ((r.serName).Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    data += "<tr><td><img width='100' height='100' src='" + r.serImage + "'" + r.serName + "' /></td><td>" + r.serName + "</td><td>" + r.serBaseCost + "</td><td>" + r.serType + "</td><td>"
+                        + r.serDescription + "</td><td>" + r.minLenOfService + "</td><td>" + r.maxLenOfService + "</td></tr>";
+                }
+            }
+            lblData.Text += data + "</tbody></table>";
+        }
+
+        // handles changing a service
+        protected void ChangeSelection(object sender, EventArgs e)
+        {
+            string fieldToBeChanged = fieldchanged.SelectedValue;
+            // displays selected field to be changed
+            if(fieldToBeChanged.Equals("serName")) {
+                ChangeName.Visible = true;
+                servicefield.Text = "";
+            } else if(fieldToBeChanged.Equals("serDescription"))
+            {
+                ChangeDescription.Visible = true;
+                servicefield.Text = "";
+            } else if(fieldToBeChanged.Equals("serBaseCost"))
+            {
+                ChangeBaseCost.Visible = true;
+                servicefield.Text = "";
+            } else if(fieldToBeChanged.Equals("minLenOfService"))
+            {
+                ChangeMin.Visible = true;
+                servicefield.Text = "";
+            } else if(fieldToBeChanged.Equals("maxLenOfService"))
+            {
+                ChangeMax.Visible = true;
+                servicefield.Text = "";
+            } else if(fieldToBeChanged.Equals("serImage"))
+            {
+                ChangeImg.Visible = true;
+                servicefield.Text = "";
+            } else if(fieldToBeChanged.Equals("serType"))
+            {
+                ChangeType.Visible = true;
+                servicefield.Text = "";
+            }
+            ChangeButtonDiv.Visible = true;
+        }
+
+        // updates selected service
+        protected void updateService(object sender, EventArgs e)
+        {
+            string NameOfServiceToBeChanged = ServiceNameF.SelectedValue;
+            string FieldToBeChanged = fieldchanged.SelectedValue;
+            string ValueToBeChanged;
+            string strQuery = "";
+            // updates selected field in database
+            if (FieldToBeChanged.Equals("serName"))
+            {
+                ValueToBeChanged = NewName.Value;
+                 strQuery = "Update tblService Set serName='" + ValueToBeChanged + "' Where serName='" + NameOfServiceToBeChanged + "'";
+            }
+            else if (FieldToBeChanged.Equals("serDescription"))
+            {
+                ValueToBeChanged = NewDescription.Value;
+                 strQuery = "Update tblService Set serDescription='" + ValueToBeChanged + "' Where serName='" + NameOfServiceToBeChanged + "'";
+            }
+            else if(FieldToBeChanged.Equals("serBaseCost"))
+            {
+                ValueToBeChanged = NewCost.Value;
+                 strQuery = "Update tblService Set serBaseCost='" + Int32.Parse(ValueToBeChanged) + "' Where serName='" + NameOfServiceToBeChanged + "'";
+            }
+            else if(FieldToBeChanged.Equals("minLenOfService"))
+            {
+                ValueToBeChanged = NewMin.Value;
+                 strQuery = "Update tblService Set minLenOfService='" + Int32.Parse(ValueToBeChanged) + "' Where serName='" + NameOfServiceToBeChanged + "'";
+            }
+            else if(FieldToBeChanged.Equals("maxLenOfService"))
+            {
+                ValueToBeChanged = NewMax.Value;
+                strQuery = "Update tblService Set maxLenOfService='" + Int32.Parse(ValueToBeChanged) + "' Where serName='" + NameOfServiceToBeChanged + "'";
+
+            }
+            else if(FieldToBeChanged.Equals("serImage"))
+            {
+                ValueToBeChanged = "~pictures/" + NewImage.Value;
+                strQuery = "Update tblService Set serImage='" + ValueToBeChanged + "' Where serName='" + NameOfServiceToBeChanged + "'";
+
+            }
+            else if(FieldToBeChanged.Equals("serType"))
+            {
+                if (Cleaning.Checked)
+                {
+                    ValueToBeChanged = "cleaning";
+                    strQuery = "Update tblService Set serType='" + ValueToBeChanged + "' Where serName='" + NameOfServiceToBeChanged + "'";
+
+                }
+                else if (Party.Checked)
+                {
+                    ValueToBeChanged = "party";
+                    strQuery = "Update tblService Set serType='" + ValueToBeChanged + "' Where serName='" + NameOfServiceToBeChanged + "'";
+
+                }
+                else
+                {
+                    ValueToBeChanged = "other";
+                    strQuery = "Update tblService Set serType='" + ValueToBeChanged + "' Where serName='" + NameOfServiceToBeChanged + "'";
+
+                }
+            }
+            string finalconnect = string.Empty;
+            finalconnect = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(finalconnect))
+            {
+                using (SqlCommand command = new SqlCommand(strQuery, connection))
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    if (result < 0)
+                    {
+                        System.Diagnostics.Debug.WriteLine("whats the problem bobby");
+                    }
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+        // adds subscription service to database
+        protected void addSubscription(object sender, EventArgs e)
+        {
+            // sets types
+            string subscriptionType = "";
+            if (subCleaning.Checked)
+            {
+                subscriptionType = "cleaning";
+            }
+            else if (subParty.Checked)
+            {
+                subscriptionType = "party";
+            }
+            else
+            {
+                subscriptionType = "other";
+            }
+            // sets image
+            string imageLocation = "pictures/" + serImage.Value;
+            // adds to database
+            string finalconnect = string.Empty;
+            finalconnect = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(finalconnect))
+            {
+                string strQuery = "INSERT INTO subService (serName, serType, serImage, serDescription, serWeeklyCost)" +
+                    " VALUES (@SubName, @subType, @subImage, @subDescription, @subWeeklyCost)";
+                using (SqlCommand command = new SqlCommand(strQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@SubName", SubName.Value);
+                    command.Parameters.AddWithValue("@subType", subscriptionType);
+                    command.Parameters.AddWithValue("@subImage", subImg);
+                    command.Parameters.AddWithValue("@subDescription", subDescription.Value);
+                    command.Parameters.AddWithValue("@subWeeklyCost", Int32.Parse(weeklyCost.Value));
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    if (result < 0)
+                    {
+                        System.Diagnostics.Debug.WriteLine("whats the problem bobby");
+                    }
+
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+            string folderPath = Server.MapPath("~/pictures/");
+            string fileName = serImage.Value;
+            string imagePath = folderPath + fileName;
+        }
+
+        // displays all service bookings
+        protected void viewAllBookings()
+        {
+            // retrieves bookings from database
+            string finalconnect = string.Empty;
+            finalconnect = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(finalconnect))
+            {
+                string strQuery = "SELECT * FROM tblDates";
+                using (SqlCommand command = new SqlCommand(strQuery, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    BookedGrid.DataSource = reader;
+                    BookedGrid.DataBind();
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+    }
+}
